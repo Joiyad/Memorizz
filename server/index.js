be397c668ffbdd7983f160b372ee8cdf71d3bd2c
Joiyad <20bee012@nith.ac.in>
2023-02-01 01:25:48 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -23,11 +23,11 @@ app.get('/', (req, res) => {
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-.catch((error) => console.log(error.message))
-
 mongoose.set("strictQuery", false);
-mongoose.set('useFindModify', false);
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+// mongoose.set('useFindModify', false);
 
 
