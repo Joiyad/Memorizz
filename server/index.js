fe1ba391e46d3e0771b74152f7e3becaa234d6f7
Joiyad <20bee012@nith.ac.in>
2023-02-01 01:02:22 +0530

    error resolved

--- server/index.js
+++ server/index.js
@@ -23,11 +23,11 @@ app.get('/', (req, res) => {
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
-mongoose.set("strictQuery", false);
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-    .catch((error) => console.log(error.message))
+.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+.catch((error) => console.log(error.message))
 
-// mongoose.set('useFindModify', false);
+mongoose.set("strictQuery", false);
+mongoose.set('useFindModify', false);
 
 
